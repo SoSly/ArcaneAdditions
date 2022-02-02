a283b314848336485333ceebb960613bf8ca657d
Kevin Kragenbrink <kevin@writh.net>
2022-02-02 11:42:29 -0800

    docs(guidebook): add guidebook entries for polymorph (and ice block)

--- src/main/java/org/sosly/witchesandwizards/events/SetupEventRegistry.java
+++ src/main/java/org/sosly/witchesandwizards/events/SetupEventRegistry.java
@@ -1,11 +1,13 @@
 package org.sosly.witchesandwizards.events;
 
+import com.mna.api.guidebook.RegisterGuidebooksEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.sosly.witchesandwizards.WitchesAndWizards;
 import org.sosly.witchesandwizards.client.model.IceBlockModel;
+import org.sosly.witchesandwizards.utils.RLoc;
 
 @Mod.EventBusSubscriber(modid = WitchesAndWizards.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class SetupEventRegistry {
