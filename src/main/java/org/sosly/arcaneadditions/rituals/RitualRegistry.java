372702a8aa535c1bc8b9482e7c25698cf3471f5f
Kevin Kragenbrink <kevin@writh.net>
2024-03-06 16:37:41 -0800

    feat(ritual): add a ritual of familiar binding

---
+++ src/main/java/org/sosly/arcaneadditions/rituals/RitualRegistry.java
@@ -0,0 +1,19 @@
+package org.sosly.arcaneadditions.rituals;
+
+import com.mna.Registries;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegisterEvent;
+import org.sosly.arcaneadditions.ArcaneAdditions;
+import org.sosly.arcaneadditions.rituals.effects.BindFamiliarRitual;
+import org.sosly.arcaneadditions.utils.RLoc;
+
+@Mod.EventBusSubscriber(modid = ArcaneAdditions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RitualRegistry {
+    @SubscribeEvent
+    public static void registerRitualEffects(RegisterEvent event) {
+        event.register(Registries.RitualEffect.get().getRegistryKey(), helper -> {
+            helper.register(RLoc.create("ritual-bind-familiar"), new BindFamiliarRitual(RLoc.create("rituals/bind-familiar")));
+        });
+    }
+}
