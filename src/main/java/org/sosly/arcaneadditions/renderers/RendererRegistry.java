64559c331a98df2800f3d45247e29f409b859733
Kevin Kragenbrink <kevin@writh.net>
2024-03-07 15:04:08 -0800

    fix: ensure client events only run on the client

--- src/main/java/org/sosly/arcaneadditions/renderers/RendererRegistry.java
+++ src/main/java/org/sosly/arcaneadditions/renderers/RendererRegistry.java
@@ -10,19 +10,21 @@ package org.sosly.arcaneadditions.renderers;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.sosly.arcaneadditions.ArcaneAdditions;
 import org.sosly.arcaneadditions.entities.EntityRegistry;
 import org.sosly.arcaneadditions.renderers.sorcery.AstralProjectionRenderer;
 import org.sosly.arcaneadditions.renderers.sorcery.IceBlockRenderer;
 import org.sosly.arcaneadditions.renderers.sorcery.SoulSearchersBeamRenderer;
 
-;
 
+@Mod.EventBusSubscriber(modid = ArcaneAdditions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class RendererRegistry {
-    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
-    public static void registerEntityRenderers(FMLClientSetupEvent event) {
+    public static void setupRenderers(EntityRenderersEvent.RegisterRenderers event) {
         EntityRenderers.register(EntityRegistry.ASTRAL_PROJECTION.get(), (context) -> new AstralProjectionRenderer(context, false));
         EntityRenderers.register(EntityRegistry.SOUL_SEARCHERS_BEAM.get(), SoulSearchersBeamRenderer::new);
         EntityRenderers.register(EntityRegistry.ICE_BLOCK.get(), IceBlockRenderer::new);
