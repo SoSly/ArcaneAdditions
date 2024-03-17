db9105e6fac1f9e5d3711764912bcb4544291e1b
Kevin Kragenbrink <kevin@writh.net>
2024-03-16 23:52:26 -0700

    fix: properly implement isValid for the treestride menu

--- src/main/java/org/sosly/arcaneadditions/gui/menus/TreeStrideMenu.java
+++ src/main/java/org/sosly/arcaneadditions/gui/menus/TreeStrideMenu.java
@@ -7,28 +7,48 @@
 
 package org.sosly.arcaneadditions.gui.menus;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import org.sosly.arcaneadditions.gui.menus.MenuRegistry;
+import org.sosly.arcaneadditions.capabilities.treestride.ITreestrideCapability;
+import org.sosly.arcaneadditions.capabilities.treestride.TreestrideProvider;
 
 public class TreeStrideMenu extends AbstractContainerMenu {
+    ITreestrideCapability capability;
+
     public TreeStrideMenu(int id, Inventory playerInv) {
         this(id, playerInv, playerInv.player);
+
     }
 
     public TreeStrideMenu(int id, Inventory playerInv, Player player) {
         super(MenuRegistry.TREE_STRIDE.get(), id);
+        player.level().getCapability(TreestrideProvider.TREESTRIDE).ifPresent(cap -> {
+            capability = cap;
+        });
     }
 
     @Override
-    public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
+    public ItemStack quickMoveStack(Player player, int pIndex) {
         return null;
     }
 
     @Override
-    public boolean stillValid(Player pPlayer) {
-        return true;
+    public boolean stillValid(Player player) {
+        if (player.level().isClientSide()) {
+            return true;
+        }
+
+        if (capability == null) {
+            return false;
+        }
+
+        BlockPos playerPos = player.getOnPos();
+        BlockPos rootPos = capability.getCurrentPosition((ServerPlayer) player);
+
+        return rootPos != null && playerPos.distManhattan(rootPos) < player.getBlockReach();
     }
 }
